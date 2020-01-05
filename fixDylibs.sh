da0c59ff753e97c4b944f071d2a617b09bac0cd8
Ethan Lee <flibitijibibo@gmail.com>
2020-01-05 12:45:29 -0500

    Use @rpath instead of @executable_path

--- fixDylibs.sh
+++ fixDylibs.sh
@@ -7,14 +7,12 @@ do
     # a given dynamic library. It will then try to find the library
     # at that location. This usually defaults to somewhere in the
     # system folders (e.g. /Library/Frameworks/... or /usr/lib/...)
-    # 
-    # However, since we're packaging 3rd party libraries into the
-    # app bundle, we want to tell OS X's dynamic linker to look
-    # in the executable's path + osx subfolder (where we've told
-    # XCode to copy our dynamic libraries.)
-    install_name_tool -id @executable_path/osx/`basename $f` osx/$f
-    install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @executable_path/osx/libSDL2-2.0.0.dylib osx/$f
+    #
+    # Instead, we want to set @rpath in the executable, then fix the paths in
+    # the libraries to use @rpath for their link paths.
+    install_name_tool -id @rpath/`basename $f` osx/$f
+    install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @rpath/libSDL2-2.0.0.dylib osx/$f
 
-    # You should see @executable_path/osx/LIBNAME here
+    # You should see @rpath/LIBNAME here
     otool -L osx/$f
 done
